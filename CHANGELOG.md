418bd1fdca3ed58160fc1130648b7b2e44daca02
HansKrywaa <hanskrywaa@web.de>
2020-08-28 10:19:35 +0200

    github username changed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
-## ChangeLog
+# ChangeLog
 
-### 1.1.0
+## 1.1.1
+- I changed my Github Username --> Links & Android Package Name adjusted
+
+## 1.1.0
 - Method `includeToTaskList` added
--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -1,25 +1,4 @@
-/*
- Copyright 2013 Sebastián Katzer
-
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
- */
-
-package de.hanskrywaa;
+package de.einfachhans.BackgroundMode;
 
 import android.app.Activity;
 import android.content.ComponentName;
@@ -34,10 +13,10 @@ import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import de.hanskrywaa.ForegroundService.ForegroundBinder;
+import de.einfachhans.BackgroundMode.ForegroundService.ForegroundBinder;
 
 import static android.content.Context.BIND_AUTO_CREATE;
-import static de.hanskrywaa.BackgroundModeExt.clearKeyguardFlags;
+import static de.einfachhans.BackgroundMode.BackgroundModeExt.clearKeyguardFlags;
 
 public class BackgroundMode extends CordovaPlugin {
 
